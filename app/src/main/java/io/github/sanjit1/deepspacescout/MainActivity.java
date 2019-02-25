cc9b54f79bf4a2f4053eb14c365e7bdea5f894a4
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-02-24 21:33:13 -0800

    sorry it made me do stuff cause of uploading to playstore and i changed the package name previoous one was spelt wrong

---
+++ app/src/main/java/io/github/sanjit1/deepspacescout/MainActivity.java
@@ -0,0 +1,84 @@
+package io.github.sanjit1.deepspacescout;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.app.Activity;
+import android.content.Intent;
+//import android.view.Menu;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+//import android.view.View.OnClickListener;
+//import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+// request permission, for internet access, read and write storage if it has not been already given
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            }, 1);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_NETWORK_STATE
+            }, 1);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.INTERNET
+            }, 1);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.INTERNET}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+        File scout = new File((Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)) + "/ScouterAppData");
+        createDir(scout);
+        scout = new File((Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)) + "/ScouterAppData/ActivityData");
+        createDir(scout);
+
+    }
+        public void createDir(File scout){
+            if (!scout.exists() && !scout.isDirectory()) {
+                // create empty directory
+                if (scout.mkdirs()) {
+                    Log.i("CreateDir", "App dir created");
+                } else {
+
+                }
+            } else {
+                Log.i("CreateDir", "App dir already exists");
+            }
+        }
+
+
+        public void onClick (View button){
+        if(button.getId()==R.id.scout){
+                Intent myIntent = new Intent(MainActivity.this,
+                        TeamName.class);
+                startActivity(myIntent);
+
+        } else if(button.getId()==R.id.createYourOwn) {
+            Intent myIntent = new Intent(MainActivity.this,
+                    DIY.class);
+            startActivity(myIntent);
+
+        }
+
+    }
+
+}
+
